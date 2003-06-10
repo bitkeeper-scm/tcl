c9a988500a5a2b2820a1dc8970d267d43792d069
msofer <msofer@tcl.tk>
2003-06-10 19:52:58 +0000

    tclExecute.c, tclBasic.c, interp.test, ChangeLog:   	* generic/tclBasic.c:   	* generic/tclExecute.c: let TclExecuteObjvInternal call   	TclInterpReady instead of relying on its callers to do so; fix for   	the part of [Bug 495830] that is new in 8.4.   	* tests/interp.test: Added tests 18.9 (knownbug) and 18.10 tclExecute.c:   fix for previous commit
    bk: 3ee6371aIs4OvwuJPL6NzvXVDhyT5A

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -894,7 +894,9 @@ TclCompEvalObj(interp, objPtr)
      * Check that the interpreter is ready to execute scripts
      */
 
+    iPtr->numLevels++;
     if (TclInterpReady(interp) == TCL_ERROR) {
+	iPtr->numLevels--;
 	return TCL_ERROR;
     }
 
@@ -916,6 +918,7 @@ TclCompEvalObj(interp, objPtr)
 	iPtr->errorLine = 1; 
 	result = tclByteCodeType.setFromAnyProc(interp, objPtr);
 	if (result != TCL_OK) {
+	    iPtr->numLevels--;
 	    return result;
 	}
 	iPtr->evalFlags = 0;
@@ -980,9 +983,7 @@ TclCompEvalObj(interp, objPtr)
 	    iPtr->returnOpts = iPtr->defaultReturnOpts;
 	    Tcl_IncrRefCount(iPtr->returnOpts);
 	}
-	iPtr->numLevels++;
 	result = TclExecuteByteCode(interp, codePtr);
-	iPtr->numLevels--;
 	codePtr->refCount--;
 	if (codePtr->refCount <= 0) {
 	    TclCleanupByteCode(codePtr);
@@ -990,6 +991,8 @@ TclCompEvalObj(interp, objPtr)
     } else {
 	result = TCL_OK;
     }
+    iPtr->numLevels--;
+
 
     /*
      * If no commands at all were executed, check for asynchronous
