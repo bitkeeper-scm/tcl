6ed4906802b2234ac886e6ce39ba59f6a3fc3376
mig <mig@uh.(none)>
2007-11-20 00:43:18 -0300

    simplify deep code for hashes; something remains fishy (grep for FIXME AND EXPLAIN) improved leak tests
    bk: 474257d67xKdIDd0OHpL5Scy1pdoig

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -2162,8 +2162,7 @@ typedef struct List {
  *
  * DICT_PATH_UPDATE indicates that we are going to be doing an update at the
  * tip of the path, so duplication of shared objects should be done along the
- * way. If the DICT_PATH_KILL_STRING flag is set the string reps along the
- * path are reset.
+ * way. 
  *
  * DICT_PATH_EXISTS indicates that we are performing an existance test and a
  * lookup failure should therefore not be an error. If (and only if) this flag
@@ -2178,7 +2177,6 @@ typedef struct List {
 #define DICT_PATH_UPDATE	1
 #define DICT_PATH_EXISTS	2
 #define DICT_PATH_CREATE	5
-#define DICT_PATH_KILL_STRING   8
 
 #define DICT_PATH_NON_EXISTENT	((Tcl_Obj *) (void *) 1)
 
