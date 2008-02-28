5e786926f1b3aca993c1b065dd59b2db2fd869fe
jenglish <jenglish@tcl.tk>
2008-02-28 20:14:12 +0000

    tclUnixChan.c, tclUnixCompat.c, tclUnixNotfy.c, tclUnixPipe.c, tclUnixPort.h:   Consolidate all code conditionalized on -DUSE_FIONBIO into one place.   New routine TclUnixSetBlockingMode(). ChangeLog, tclUnixChan.c, tclUnixCompat.c, tclUnixPort.h:   Reduce scope of <sys/filio.h> and <sys/ioctl.h> #includes.
    bk: 47c71614jbrwyoNOpIIcZDdC3G_PxQ

--- unix/tclUnixPort.h
+++ unix/tclUnixPort.h
@@ -108,20 +108,9 @@ typedef off_t		Tcl_SeekOffset;
 #else
 #   include "../compat/unistd.h"
 #endif
-#ifdef	USE_FIONBIO
-    /*
-     * Not using the Posix fcntl(...,O_NONBLOCK,...) interface, instead
-     * we are using ioctl(..,FIONBIO,..).
-     */
 
-#   ifdef HAVE_SYS_FILIO_H
-#	include	<sys/filio.h>	/* For FIONBIO. */
-#   endif
+MODULE_SCOPE int TclUnixSetBlockingMode(int fd, int mode);
 
-#   ifdef HAVE_SYS_IOCTL_H
-#	include	<sys/ioctl.h>	/* For FIONBIO. */
-#   endif
-#endif	/* USE_FIONBIO */
 #include <utime.h>
 
 /*
